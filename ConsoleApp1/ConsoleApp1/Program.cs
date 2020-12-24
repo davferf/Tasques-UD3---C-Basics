841b5a3d7e25e20713aae7f168ae1bcc3b82ceb1
David Ferrer <dferrerfernandez@gmail.com>
2020-12-24 12:06:05 +0100

    Ejercicio 5

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 1, B = 2, C = 3, D = 4, aux;
+
+            aux = B;
+            B = C;
+            C = A;
+            A = D;
+            D = aux;
+
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(C);
+            Console.WriteLine(D);
+
+            Console.ReadKey();
+        }
+    }
+}
