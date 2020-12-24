5daaf0374f082c1bb69e600cb16f2fd8f7b0eb68
David Ferrer <dferrerfernandez@gmail.com>
2020-12-24 11:55:45 +0100

    Ejercicio 4

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 16;
+
+            Console.WriteLine(N += 77);
+            Console.WriteLine(N -= 3);
+            Console.WriteLine(N *= 2);
+
+            Console.ReadKey();
+        }
+    }
+}
