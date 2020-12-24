5cf2add4976a17e0daef3091d4a14c8c4fbd6fc1
David Ferrer <dferrerfernandez@gmail.com>
2020-12-24 10:54:08 +0100

    Ejercicio2

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 36;
+            double A = 75.87;
+            char C = 'a';
+
+            Console.WriteLine(N);
+            Console.WriteLine(A);
+            Console.WriteLine(C);
+
+            Console.WriteLine(N + A);
+            Console.WriteLine(A - N);
+
+            Console.WriteLine((int) C);
+
+            Console.ReadKey();
+        }
+    }
+}
