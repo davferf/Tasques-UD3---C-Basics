1242cca838aafc16c610d113d86b0dc46b92d842
David Ferrer <dferrerfernandez@gmail.com>
2020-12-24 10:29:42 +0100

    Ejercicio1
    Ejercicio1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 64;
+            int num2 = 256;
+
+            Console.WriteLine(num1 + num2);
+            Console.WriteLine(num1 - num2);
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine(num1 / num2);
+            Console.WriteLine(num1 % num2);
+
+            Console.ReadKey();
+
+        }
+    }
+}
